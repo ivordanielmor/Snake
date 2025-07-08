d2cad43bf356127a0ff763d85910133552b4bbe7
Daniel Mór Ivor <ivordanielmor@gmail.com>
2025-07-08 19:21:58 +0200

    Hozzáadva: Folyamatos mozgás

--- snake.py
+++ snake.py
@@ -1,16 +1,21 @@
-# 3. Billentyűkezelés - A kurzor nyilai 20 pixellel mozgatják a kígyó fejét.
+# HÁZI FELADAT: Állítsd be ,hogy a kígyó folyamatosan mozogjon (ne csak a billentyűlenyomásra)
+#  és a mozgás irányát a legutolsó nyílbillentyű-esemény határozza meg.
 
 import pygame
 
 pygame.init()
 
-kepernyo = pygame.display.set_mode((600, 400))
+kepernyo = pygame.display.set_mode((1920, 1080))
 
 ora = pygame.time.Clock()
 
 kigyo_meret = 20
-kigyo_x = (600 - kigyo_meret) // 2
-kigyo_y = (400 - kigyo_meret) // 2
+kigyo_x = (1920 - kigyo_meret) // 2
+kigyo_y = (1080 - kigyo_meret) // 2
+
+# Mozgásirány változók (sebesség pixelben)
+sebesseg_x = 20
+sebesseg_y = 0
 
 zold = (0, 255, 0)
 
@@ -18,25 +23,31 @@ fut = True
 
 # Játék fő ciklusa
 while fut:
-    # Események kezelése (pl. kilépés)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             fut = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                kigyo_x -= 20
+                sebesseg_x = -20
+                sebesseg_y = 0
             if event.key == pygame.K_RIGHT:
-                kigyo_x += 20
+                sebesseg_x = 20
+                sebesseg_y = 0
             if event.key == pygame.K_UP:
-                kigyo_y -= 20
+                sebesseg_x = 0
+                sebesseg_y = -20
             if event.key == pygame.K_DOWN:
-                kigyo_y += 20
+                sebesseg_x = 0
+                sebesseg_y = 20
+
+    kigyo_x += sebesseg_x
+    kigyo_y += sebesseg_y
 
     # Képernyő törlés
     kepernyo.fill((0, 0, 0))
 
     pygame.draw.rect(kepernyo, zold, (kigyo_x, kigyo_y, kigyo_meret, kigyo_meret))
     pygame.display.flip()
-    ora.tick(60)
+    ora.tick(10)
 
 pygame.quit() 
